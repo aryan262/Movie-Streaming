6ee53f2d9307ee174f093a43898d3336c60d278c
aryan262 <aryan.nikolas@gmail.com>
2024-08-09 09:10:33 +0530

    changed targed in vite.config.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({ origin: "http://your-frontend-domain.com" }));
 app.use(express.urlencoded({extended:true}))
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
